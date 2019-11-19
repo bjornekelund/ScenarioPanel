d29fe22887de21f6a9e72d26b49dcca4f8bef5f4
Björn Ekelund <bjorn@ekelund.nu>
2019-11-20 00:28:02 +0100

    Works

--- DXLCusForm1/CusFrmTest1.Designer.cs
+++ DXLCusForm1/CusFrmTest1.Designer.cs
@@ -48,89 +48,98 @@
             // 
             // label1
             // 
-            this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(15, 13);
+            this.label1.Font = new System.Drawing.Font("Courier New", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(7, 4);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(35, 13);
+            this.label1.Size = new System.Drawing.Size(130, 18);
             this.label1.TabIndex = 2;
-            this.label1.Text = "label1";
+            this.label1.Text = "Heavy pile up";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
             this.label1.Click += new System.EventHandler(this.label1_Click);
             // 
             // label2
             // 
-            this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(15, 36);
+            this.label2.Font = new System.Drawing.Font("Courier New", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(7, 29);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(35, 13);
+            this.label2.Size = new System.Drawing.Size(130, 18);
             this.label2.TabIndex = 3;
             this.label2.Text = "label2";
+            this.label2.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
             this.label2.Click += new System.EventHandler(this.label2_Click);
             // 
             // label3
             // 
-            this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(15, 60);
+            this.label3.Font = new System.Drawing.Font("Courier New", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(7, 54);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(35, 13);
+            this.label3.Size = new System.Drawing.Size(130, 18);
             this.label3.TabIndex = 4;
             this.label3.Text = "label3";
+            this.label3.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
             this.label3.Click += new System.EventHandler(this.label3_Click);
             // 
             // label4
             // 
-            this.label4.AutoSize = true;
-            this.label4.Location = new System.Drawing.Point(15, 85);
+            this.label4.Font = new System.Drawing.Font("Courier New", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(7, 79);
             this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(35, 13);
+            this.label4.Size = new System.Drawing.Size(130, 18);
             this.label4.TabIndex = 5;
             this.label4.Text = "label4";
+            this.label4.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
             this.label4.Click += new System.EventHandler(this.label4_Click);
             // 
             // label5
             // 
-            this.label5.AutoSize = true;
-            this.label5.Location = new System.Drawing.Point(15, 111);
+            this.label5.Font = new System.Drawing.Font("Courier New", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(7, 104);
             this.label5.Name = "label5";
-            this.label5.Size = new System.Drawing.Size(35, 13);
+            this.label5.Size = new System.Drawing.Size(130, 18);
             this.label5.TabIndex = 6;
             this.label5.Text = "label5";
+            this.label5.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
             this.label5.Click += new System.EventHandler(this.label5_Click);
             // 
             // label6
             // 
-            this.label6.AutoSize = true;
-            this.label6.Location = new System.Drawing.Point(15, 139);
+            this.label6.Font = new System.Drawing.Font("Courier New", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.Location = new System.Drawing.Point(7, 129);
             this.label6.Name = "label6";
-            this.label6.Size = new System.Drawing.Size(35, 13);
+            this.label6.Size = new System.Drawing.Size(130, 18);
             this.label6.TabIndex = 7;
             this.label6.Text = "label6";
+            this.label6.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
             this.label6.Click += new System.EventHandler(this.label6_Click);
             // 
             // label7
             // 
-            this.label7.AutoSize = true;
-            this.label7.Location = new System.Drawing.Point(15, 166);
+            this.label7.Font = new System.Drawing.Font("Courier New", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label7.Location = new System.Drawing.Point(7, 154);
             this.label7.Name = "label7";
-            this.label7.Size = new System.Drawing.Size(35, 13);
+            this.label7.Size = new System.Drawing.Size(130, 18);
             this.label7.TabIndex = 8;
             this.label7.Text = "label7";
+            this.label7.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
             this.label7.Click += new System.EventHandler(this.label7_Click);
             // 
             // label8
             // 
-            this.label8.AutoSize = true;
-            this.label8.Location = new System.Drawing.Point(15, 190);
+            this.label8.Font = new System.Drawing.Font("Courier New", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label8.Location = new System.Drawing.Point(7, 179);
             this.label8.Name = "label8";
-            this.label8.Size = new System.Drawing.Size(35, 13);
+            this.label8.Size = new System.Drawing.Size(130, 18);
             this.label8.TabIndex = 9;
             this.label8.Text = "label8";
+            this.label8.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
             this.label8.Click += new System.EventHandler(this.label8_Click);
             // 
             // CusFrmTest1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(284, 262);
+            this.BackColor = System.Drawing.Color.Turquoise;
+            this.ClientSize = new System.Drawing.Size(149, 206);
             this.Controls.Add(this.label8);
             this.Controls.Add(this.label7);
             this.Controls.Add(this.label6);
@@ -145,9 +154,8 @@
             this.MaximizeBox = false;
             this.MinimizeBox = false;
             this.Name = "CusFrmTest1";
-            this.Text = "My custom form";
+            this.Text = "Scenario Panel";
             this.ResumeLayout(false);
-            this.PerformLayout();
 
         }
 
--- DXLCusForm1/CusFrmTest1.cs
+++ DXLCusForm1/CusFrmTest1.cs
@@ -15,13 +15,13 @@ namespace DXLog.net
         {
             get { return 1011; }
         }
-        
-        //private ContestData _cdata = null;
+
+        private ContestData _cdata = null;
         private Font _windowFont = new Font("Courier New", 10, FontStyle.Regular);
 
         private FrmMain mainForm = null;
 
-        private delegate void newQsoSaved(DXQSO qso);
+        private delegate void ScenarioSelectionChange(int newScenario);
 
         public CusFrmTest1()
         {
@@ -33,7 +33,7 @@ namespace DXLog.net
             InitializeComponent();
             ColorSetTypes = new string[] { "Background", "Color", "Header back color", "Header color", "Footer back color", "Footer color", "Final score color", "Selection back color", "Selection color" };
             DefaultColors = new Color[] { Color.Turquoise, Color.Black, Color.Gray, Color.Black, Color.Silver, Color.Black, Color.Blue, Color.SteelBlue, Color.White };
-            //_cdata = cdata;
+            _cdata = cdata;
             FormLayoutChangeEvent += new FormLayoutChange(handle_FormLayoutChangeEvent);
         }
 
@@ -55,17 +55,103 @@ namespace DXLog.net
                 mainForm = (FrmMain)(ParentForm == null ? Owner : ParentForm);
                 if (mainForm != null)
                 {
-                    label1.Text = mainForm.so2rScenarios[0].ScenarioName.Trim();
-                    label2.Text = mainForm.so2rScenarios[1].ScenarioName.Trim();
-                    label3.Text = mainForm.so2rScenarios[2].ScenarioName.Trim();
-                    label4.Text = mainForm.so2rScenarios[3].ScenarioName.Trim();
-                    label5.Text = mainForm.so2rScenarios[4].ScenarioName.Trim();
-                    label6.Text = mainForm.so2rScenarios[5].ScenarioName.Trim();
-                    label7.Text = mainForm.so2rScenarios[6].ScenarioName.Trim();
-                    label8.Text = mainForm.so2rScenarios[7].ScenarioName.Trim();
+                    //_cdata.ScenarioDefinitionChanged += new ContestData.ScenarioDefinitionChange(update_display_c);
+                    _cdata.ScenarioSelectionChanged += new ContestData.ScenarioSelectionChange(update_display);
+                    update_display(mainForm.activeSO2RScenario + 1);
                 }
             }
         }
+
+        //private void update_display_c()
+        //{
+        //    update_display(mainForm.activeSO2RScenario);
+        //}
+
+        private void update_display(int scenario)
+        {
+            //    if (InvokeRequired)
+            //    {
+            //        newQsoSaved d = new newQsoSaved(mainForm_NewQSOSaved);
+            //        Invoke(d, new object[] { newQso });
+            //        return;
+            //    }
+
+            if (InvokeRequired)
+            {
+                ContestData.ScenarioSelectionChange d = new ContestData.ScenarioSelectionChange(update_display);
+                Invoke(d, new object[] { scenario });
+                return;
+            }
+
+            label1.Text = mainForm.so2rScenarios[0].ScenarioName.Trim();
+            label2.Text = mainForm.so2rScenarios[1].ScenarioName.Trim();
+            label3.Text = mainForm.so2rScenarios[2].ScenarioName.Trim();
+            label4.Text = mainForm.so2rScenarios[3].ScenarioName.Trim();
+            label5.Text = mainForm.so2rScenarios[4].ScenarioName.Trim();
+            label6.Text = mainForm.so2rScenarios[5].ScenarioName.Trim();
+            label7.Text = mainForm.so2rScenarios[6].ScenarioName.Trim();
+            label8.Text = mainForm.so2rScenarios[7].ScenarioName.Trim();
+
+            label1.BackColor = Color.Turquoise;
+            label2.BackColor = Color.Turquoise;
+            label3.BackColor = Color.Turquoise;
+            label4.BackColor = Color.Turquoise;
+            label5.BackColor = Color.Turquoise;
+            label6.BackColor = Color.Turquoise;
+            label7.BackColor = Color.Turquoise;
+            label8.BackColor = Color.Turquoise;
+
+            label1.ForeColor = Color.Black;
+            label2.ForeColor = Color.Black;
+            label3.ForeColor = Color.Black;
+            label4.ForeColor = Color.Black;
+            label5.ForeColor = Color.Black;
+            label6.ForeColor = Color.Black;
+            label7.ForeColor = Color.Black;
+            label8.ForeColor = Color.Black;
+
+            if (_cdata.OPTechnique == ContestData.Technique.SO2R_ADV)
+            {
+                switch (scenario)
+                {
+                    case 1:
+                        label1.BackColor = Color.Red;
+                        label1.ForeColor = Color.Yellow;
+                        break;
+                    case 2:
+                        label2.BackColor = Color.Red;
+                        label2.ForeColor = Color.Yellow;
+                        break;
+                    case 3:
+                        label3.BackColor = Color.Red;
+                        label3.ForeColor = Color.Yellow;
+                        break;
+                    case 4:
+                        label4.BackColor = Color.Red;
+                        label4.ForeColor = Color.Yellow;
+                        break;
+                    case 5:
+                        label5.BackColor = Color.Red;
+                        label5.ForeColor = Color.Yellow;
+                        break;
+                    case 6:
+                        label6.BackColor = Color.Red;
+                        label6.ForeColor = Color.Yellow;
+                        break;
+                    case 7:
+                        label7.BackColor = Color.Red;
+                        label7.ForeColor = Color.Yellow;
+                        break;
+                    case 8:
+                        label8.BackColor = Color.Red;
+                        label8.ForeColor = Color.Yellow;
+                        break;
+
+                }
+            }
+
+        }
+
 
         //private void mainForm_NewQSOSaved(DXQSO newQso)
         //{
@@ -87,42 +173,42 @@ namespace DXLog.net
 
         private void label1_Click(object sender, EventArgs e)
         {
-            mainForm.SelectSO2RScenario(0);
+            mainForm.SelectSO2RScenario(1);
         }
 
         private void label2_Click(object sender, EventArgs e)
         {
-            mainForm.SelectSO2RScenario(1);
+            mainForm.SelectSO2RScenario(2);
         }
 
         private void label3_Click(object sender, EventArgs e)
         {
-            mainForm.SelectSO2RScenario(2);
+            mainForm.SelectSO2RScenario(3);
         }
 
         private void label4_Click(object sender, EventArgs e)
         {
-            mainForm.SelectSO2RScenario(3);
+            mainForm.SelectSO2RScenario(4);
         }
 
         private void label5_Click(object sender, EventArgs e)
         {
-            mainForm.SelectSO2RScenario(4);
+            mainForm.SelectSO2RScenario(5);
         }
 
         private void label6_Click(object sender, EventArgs e)
         {
-            mainForm.SelectSO2RScenario(5);
+            mainForm.SelectSO2RScenario(6);
         }
 
         private void label7_Click(object sender, EventArgs e)
         {
-            mainForm.SelectSO2RScenario(6);
+            mainForm.SelectSO2RScenario(7);
         }
 
         private void label8_Click(object sender, EventArgs e)
         {
-            mainForm.SelectSO2RScenario(7);
+            mainForm.SelectSO2RScenario(8);
         }
     }
 }
